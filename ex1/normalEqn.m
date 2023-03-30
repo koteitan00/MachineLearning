9f84dc94c91f6a9badc681ff20096678aa404976
koteitan00 <hoshikairi00@gmail.com>
2023-03-30 20:01:34 +0900

    first commit

---
+++ ex1/normalEqn.m
@@ -0,0 +1,15 @@
+
+%=========parameter setting===========
+R = 1.34;
+L = 0.12;
+K_e = 7.17e-2;
+K_t =16.3e-2;
+J_m = 1.2e-4;
+
+J_i = 9.1e-7;
+D = 6.1e-5;
+
+v_m = 100;
+
+%========simulation run
+Endtime = 10;
